078e1138d56461d9c0688170fcf5805519b2cbcd
JOOWON KOH <67991485+joowon-dev@users.noreply.github.com>
2024-07-17 23:28:47 +0900

    [Silver V] Title: 숫자 빈도수, Time: 6048 ms, Memory: 32548 KB -BaekjoonHub

--- 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.py
+++ 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.py
@@ -2,10 +2,6 @@ import sys
 
 n,d = map(int,sys.stdin.readline().split())
 temp=''
-count = 0
 for i in range(n):
     temp+=str(i+1)
-for i in temp:
-    if i==str(d):
-        count+=1
-print(count)
+print(temp.count(str(d)))
